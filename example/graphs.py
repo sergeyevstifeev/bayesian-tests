55fe8130a66917c08fc2926fec4d54289f6abed8
Sergey Evstifeev <sergey.estifeev@klarna.com>
2013-03-13 17:48:29 +0100

    Add predictive graph examples for Poisson case

--- example/graphs.py
+++ example/graphs.py
@@ -17,8 +17,8 @@ def normal_stand_deviation(dist_entries):
     return math.sqrt(normal_variance(dist_entries))
 
 
-if __name__ == '__main__':
-    dist_entries = map(float, sys.argv[1:])
+def graph_predictive_normal(args):
+    dist_entries = map(float, args[1:])
     print "Distribution entries:", dist_entries
     N = len(dist_entries)
     S = sum(dist_entries)
@@ -30,7 +30,29 @@ if __name__ == '__main__':
     print "S/N =", Mean
     print "Student variance =", student_variance(N, Q, S)
     print "Normal standard deviation=", normal_stand_deviation(dist_entries)
-    print "Plot[{PDF[StudentTDistribution[", Mean, ",",\
+    print "Plot[{PDF[StudentTDistribution[", Mean, ",", \
         math.sqrt(student_variance(N, Q, S)), ",", N - 1, "], x], PDF[NormalDistribution[", \
         Mean, ",", normal_stand_deviation(dist_entries), "], x], PDF[NormalDistribution[0, 1], x]}, {x, -4, 4}]"
+
+
+def graph_predictive_poisson(args):
+    dist_entries = map(float, args[1:])
+    print "Distribution entries:", dist_entries
+    N = len(dist_entries)
+    S = sum(dist_entries)
+    Mean = S / N
+    print "N =", N
+    print "S =", S
+    print "Mean =", Mean
+    print "DiscretePlot[" \
+          "{PDF[PoissonDistribution[", Mean, "], k], " \
+          "PDF[PoissonDistribution[20], k], " \
+          "PDF[NegativeBinomialDistribution[", int(S), ", ", float(N) / (N + 1), "], k]}, " \
+          "{k, 0, 60}, " \
+          "PlotRange -> All, " \
+          "PlotMarkers -> Automatic]"
+
+
+if __name__ == '__main__':
+    graph_predictive_poisson(sys.argv)
 
